9dd69912494061b3c3715dcbbb1fd098a528ee3c
Marcin Nowakowski <marcin.nowakowski17@protonmail.com>
2025-01-17 22:57:30 +0100

    Add basic components, pages and routing.
    Add pages: Home, Login, Register, NotFound.
    Add components: LoginForm and Register for login & register logic
    and LoadingIndicator for cosmetics.
    Add some basic routing in App.jsx.

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import { useState } from "react";
+import api from "../api";
+//import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator";
+
+function LoginForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            const requestData = {username, password}
+            const res = await api.post("api/token/", { username, password })
+            localStorage.setItem(ACCESS_TOKEN, res.data.access);
+            localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+            navigate("/")
+
+        } catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>Login</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                Login
+            </button>
+        </form>
+    );
+}
+
+export default LoginForm
---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,66 @@
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import { useState } from "react";
+import api from "../api";
+//import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator";
+
+function RegisterForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            const requestData = {username, password}
+            const res = await api.post("api/user/register/", { username, password, email })
+            localStorage.setItem(ACCESS_TOKEN, res.data.access);
+            localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+            navigate("/login")
+            alert("Account registered! You can now log in.")
+
+        } catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>Register</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <input
+                className="form-input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="E-mail"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                Register
+            </button>
+        </form>
+    );
+}
+
+export default RegisterForm
