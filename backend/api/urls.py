5c1cab5e66b9bf0552278bfd42a33bef08b1f213
Marcin Nowakowski <marcin.nowakowski17@protonmail.com>
2025-01-17 19:35:44 +0100

    Fix account delete url.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("entries/", views.EntryListCreate.as_view(), name="entry-list"),
     path("entries/delete/<int:pk>/", views.EntryDelete.as_view(), name="delete-entry"),
     path("accounts/", views.AccountListCreate.as_view(), name="account-list"),
-    path("accounts/delete/<int:pk>/", views.EntryDelete.as_view(), name="delete-account"),
+    path("accounts/delete/<int:pk>/", views.AccountDelete.as_view(), name="delete-account"),
 ]
