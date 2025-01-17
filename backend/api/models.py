8653f576225bf8dd3b64f6c381193f5474926d5c
Marcin Nowakowski <marcin.nowakowski17@protonmail.com>
2025-01-17 18:09:39 +0100

    Fix typos.

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Account(models.Model):
-    name = odels.CharField(max_length=32)
+    name = models.CharField(max_length=32)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="accounts")
 
 class Category(models.Model):
@@ -13,7 +13,7 @@ class Entry(models.Model):
     title = models.CharField(max_length=100)
     value = models.DecimalField(max_digits=8, decimal_places=2)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="entries")
-    category - model.ForeignKey(Category, related_name="expenses")
+    category = models.ForeignKey(Category, null=True ,on_delete=models.SET_NULL, related_name="expenses")
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
